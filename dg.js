437a4673dd937a797a6b55e6dbc21bf572959e66
Sam Lee <samclee@gmail.com>
2018-07-01 12:07:19 -0700

    Used blocks instead of chars, implemented room carving and room selection

--- dg.js
+++ dg.js
@@ -17,40 +17,83 @@ dg.populate2d = (sz, c) => {
   return mx;
 }
 
-dg.pt = class {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
+dg.add_pts = (pt1, pt2) => ({x: pt1.x + pt2.x, y: pt1.y + pt2.y});
 
-  add(p2) {
-    return new dg.pt(this.x + p2.x, this.y + p2.y);
-  }
-}
+dg.dirs = [{x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}];
 
 dg.rand_int_in_range = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-dg.grid2map = (grid_l, rm_sz, gap) => new dg.pt(g_l * (rm_sz + gap), g_l * (rm_sz + gap));
+dg.find_valid_dir = (g_l, rm_grid) => {
+  let ret_dir = undefined;
+  let in_range = false;
 
-dg.find_open = (start_l, grid) => {
+  do {
+    ret_dir = dg.dirs[dg.rand_int_in_range(0, 4)];
+    let new_l = {x: g_l.x + ret_dir.x, y: g_l.y + ret_dir.y};
+    in_range = 0 <= new_l.x && new_l.x < rm_grid.length &&
+                0 <= new_l.y && new_l.y < rm_grid.length;
+  } while (!in_range)
 
+  return ret_dir;
 }
 
-dg.carve_rm = (grid_l, map) => {
+dg.find_open = (start_l, rm_grid) => {
+  let new_l = {x: start_l.x, y: start_l.y};
 
+  do {
+    let new_dir = dg.find_valid_dir(new_l, rm_grid);
+    new_l = dg.add_pts(new_l, new_dir);
+  } while (rm_grid[new_l.y][new_l.x] === 1)
+
+  return new_l;
 }
 
-dg.connect_rms = (grid_l1, grid_l2) => {
+dg.grid2map = (g_l, rm_sz, gap) => (
+  {x: g_l.x * (rm_sz + gap), y: g_l.y * (rm_sz + gap)}
+);
+
+dg.carve_rm = (g_l, map, rm_sz, gap) => {
+  let rm_base = dg.grid2map(g_l, rm_sz, gap);
+
+  for (let row = rm_base.y; row < rm_base.y + rm_sz; row++) {
+    for (let col = rm_base.x; col < rm_base.x + rm_sz; col++) {
+      if(row === rm_base.y || row === rm_base.y + rm_sz - 1
+          || col === rm_base.x || col === rm_base.x + rm_sz - 1)
+        map[row][col] = '#';
+      else
+        map[row][col] = '.';
+    }
+  }
+}
+
+dg.connect_rms = (g_l1, g_l2) => {
 
 }
 
 dg.gen = (num_rms, rm_sz, gap) => {
-  let max_map_sz = rm_sz * num_rms + gap * (num_rms - 1);
-  
-  let rm_grid = dg.populate2d(num_rms, 0);
+  // prepate grid and map variables
+  let max_grid_sz = Math.ceil(Math.sqrt(num_rms)) * 2; // change this to change map size
+  console.log(max_grid_sz)
+  let rm_grid = dg.populate2d(max_grid_sz, 0);
+
+  let max_map_sz = rm_sz * max_grid_sz + gap * (max_grid_sz - 1);
   let map = dg.populate2d(max_map_sz, '~');
 
+  // place center room
+  let ctr_l = {x: Math.floor(max_grid_sz / 2), y: Math.floor(max_grid_sz / 2)}; 
+  dg.carve_rm(ctr_l, map, rm_sz, gap);
+  rm_grid[ctr_l.y][ctr_l.x] = 1;
+
+  // place other rooms
   let rms_left = num_rms;
+  while (rms_left) {
+    let new_l = dg.find_open(ctr_l, rm_grid);
+    dg.carve_rm(new_l, map, rm_sz, gap);
+    rm_grid[new_l.y][new_l.x] = 1;
+
+
+    rms_left--;
+  }
   
   return map;
 }
--- main.js
+++ main.js
@@ -2,34 +2,28 @@ function init() {
   // prepate canvas
   let c = document.getElementById('display');
   let ctx = c.getContext('2d');
-  let font_sz = 1;
-  ctx.font = font_sz + 'px Monospace';
 
-  // adjust font size to fit entire map
-  let num_rms = 9;
-  let rm_sz = 3;
+  // prepare map variables
+  let num_rms = 50;
+  let rm_sz = 5;
   let gap = 0;
+  let chr_clr_map = {'~': 'lime', '#': '#ff86f4', '.': '#ffffbe'}
   
-  let max_map_sz = rm_sz * num_rms + gap * (num_rms - 1);
-  let test_str = 'a'.repeat(max_map_sz);
-
-  while (ctx.measureText(test_str).width < c.width) {
-    ctx.font = (++font_sz) + 'px Monospace';
-  }
-  console.log('Maximum font size is', font_sz);
+  // adjust sizes to fit map on screen
+  let max_grid_sz = Math.ceil(Math.sqrt(num_rms)) * 2;
+  let max_map_sz = rm_sz * max_grid_sz + gap * (max_grid_sz - 1);
+  let b_sz = c.width / max_map_sz;
 
   // display map
   let map = dg.gen(num_rms, rm_sz, gap);
 
-  let c_wdt = ctx.measureText('a').width;
-
   for (let row = 0; row < map.length; row++) {
     for (let col = 0; col < map.length; col++) {
-      if (row === (map.length - 1) || col === (map.length - 1))
-        ctx.fillStyle = 'red';
-      else
-        ctx.fillStyle = 'black';
-      ctx.fillText(map[row][col], col * c_wdt, row * font_sz * (7/12));
+      ctx.fillStyle = chr_clr_map[map[row][col]];
+      if (row === map.length - 1 || col === map.length - 1)
+        ctx.fillStyle = 'red'
+      ctx.fillRect(col * b_sz, row * b_sz, b_sz, b_sz);
+      //ctx.strokeRect(col * b_sz, row * b_sz, b_sz, b_sz);
     }
   }
 
