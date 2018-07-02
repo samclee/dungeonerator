4c0bd4fd7b4ad3df634504c1a87f1bd651a75444
Sam Lee <samclee@gmail.com>
2018-07-01 20:29:25 -0700

    Finished hallway creation
    TODO:
    -connect_hzt() and connect_vrt() are very similar, so it would be nice to merge the two
    -room merging

--- dg.js
+++ dg.js
@@ -17,6 +17,10 @@ dg.populate2d = (sz, c) => {
   return mx;
 }
 
+dg.grid2map = (g_l, rm_sz, gap) => (
+  {x: g_l.x * (rm_sz + gap), y: g_l.y * (rm_sz + gap)}
+);
+
 dg.add_pts = (pt1, pt2) => ({x: pt1.x + pt2.x, y: pt1.y + pt2.y});
 
 dg.dirs = [{x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}];
@@ -39,11 +43,57 @@ dg.find_valid_dir = (g_l, rm_grid) => {
 
 dg.copy_pt = (pt) => ({x: pt.x, y: pt.y});
 
-dg.connect_rms = (g_l1, g_l2) => {
+dg.connect_hzt = (m_l1, m_l2, map, rm_sz, gap) => {
+  let lft_most = ((m_l1.x < m_l2.x) ? m_l1.x : m_l2.x) + (rm_sz - 1);
+  let rgt_most = lft_most + gap + 1;
 
+  // fill in-between area with brick
+  for (let row = m_l1.y; row < m_l1.y + rm_sz; row++) {
+    for (let col = lft_most + 1; col < rgt_most; col ++)
+      map[row][col] = '#'
+  }
+
+  // draw main line(s)
+  for (let i = lft_most; i <= rgt_most; i++)
+    map[m_l1.y + Math.floor(rm_sz / 2)][i] = '.';
+
+  if (rm_sz % 2 === 0) {
+    for (let i = lft_most; i <= rgt_most; i++)
+      map[m_l1.y + Math.floor(rm_sz / 2) - 1][i] = '.';
+  }
 }
 
-dg.find_open = (start_l, rm_grid) => {
+dg.connect_vrt = (m_l1, m_l2, map, rm_sz, gap) => {
+  let top_most = ((m_l1.y < m_l2.y) ? m_l1.y : m_l2.y) + (rm_sz - 1);
+  let btm_most = top_most + gap + 1;
+
+  // fill in-between area with brick
+  for (let row = top_most + 1; row < btm_most; row++) {
+    for (let col = m_l1.x; col < m_l1.x + rm_sz; col ++)
+      map[row][col] = '#'
+  }
+
+  // draw main line(s)
+  for (let i = top_most; i <= btm_most; i++)
+    map[i][m_l1.x + Math.floor(rm_sz / 2)] = '.';
+
+  if (rm_sz % 2 === 0) {
+    for (let i = top_most; i <= btm_most; i++)
+      map[i][m_l1.x + Math.floor(rm_sz / 2) - 1] = '.';
+  }
+}
+
+dg.carve_tnl = (g_l1, g_l2, map, rm_sz, gap) => {
+  let m_l1 = dg.grid2map(g_l1, rm_sz, gap);
+  let m_l2 = dg.grid2map(g_l2, rm_sz, gap);
+
+  if (m_l1.y === m_l2.y)
+    dg.connect_hzt(m_l1, m_l2, map, rm_sz, gap);
+  else
+    dg.connect_vrt(m_l1, m_l2, map, rm_sz, gap);
+}
+
+dg.find_open = (start_l, rm_grid, map, rm_sz, gap) => {
   let new_l = {x: start_l.x, y: start_l.y};
   let last_l = dg.copy_pt(new_l);
 
@@ -54,14 +104,8 @@ dg.find_open = (start_l, rm_grid) => {
     new_l = dg.add_pts(new_l, new_dir);
   } while (rm_grid[new_l.y][new_l.x] === 1)
 
-  //dg.connect_rms(last_l, new_l);
-
-  return new_l;
+  return [new_l, last_l];
 }
-
-dg.grid2map = (g_l, rm_sz, gap) => (
-  {x: g_l.x * (rm_sz + gap), y: g_l.y * (rm_sz + gap)}
-);
 
 dg.carve_rm = (g_l, map, rm_sz, gap) => {
   let rm_base = dg.grid2map(g_l, rm_sz, gap);
@@ -80,7 +124,6 @@ dg.carve_rm = (g_l, map, rm_sz, gap) => {
 dg.gen = (num_rms, rm_sz, gap) => {
   // prepate grid and map variables
   let max_grid_sz = Math.ceil(Math.sqrt(num_rms) * 2); // change this to change map size
-  console.log(max_grid_sz)
   let rm_grid = dg.populate2d(max_grid_sz, 0);
 
   let max_map_sz = rm_sz * max_grid_sz + gap * (max_grid_sz - 1);
@@ -94,10 +137,19 @@ dg.gen = (num_rms, rm_sz, gap) => {
   // place other rooms
   let rms_left = num_rms;
   while (rms_left) {
-    let new_l = dg.find_open(ctr_l, rm_grid);
-    dg.carve_rm(new_l, map, rm_sz, gap);
-    rm_grid[new_l.y][new_l.x] = 1;
+    // find location of new room
+    let tnl_info = dg.find_open(ctr_l, rm_grid, map, rm_sz, gap);
+    let new_l = tnl_info[0];
+    let last_l = tnl_info[1];
 
+    // carve new room into dungeon
+    dg.carve_rm(new_l, map, rm_sz, gap);
+
+    // carve runnel between last room and current room
+    dg.carve_tnl(new_l, last_l, map, rm_sz, gap);
+
+    // record room on room grid
+    rm_grid[new_l.y][new_l.x] = 1;
 
     rms_left--;
   }
--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function init() {
   let ctx = c.getContext('2d');
 
   // prepare map variables
-  let num_rms = 50;
-  let rm_sz = 7;
+  let num_rms = 40;
+  let rm_sz = 8;
   let gap = 0;
-  let chr_clr_map = {'~': 'lime', '#': '#ff86f4', '.': '#ffffbe'}
+  let chr_clr_map = {'~': 'lime', '#': '#ff86f4', '.': '#ffffbe', '!': 'red', '@': 'blue'};
   
   // adjust sizes to fit map on screen
   let max_grid_sz = Math.ceil(Math.sqrt(num_rms) * 2);
@@ -20,9 +20,7 @@ function init() {
   for (let row = 0; row < map.length; row++) {
     for (let col = 0; col < map.length; col++) {
       ctx.fillStyle = chr_clr_map[map[row][col]];
-      //if (row === map.length - 1 || col === map.length - 1)
-      //  ctx.fillStyle = 'red'
-      ctx.fillRect(col * b_sz, row * b_sz, b_sz, b_sz);
+      ctx.fillRect(col * b_sz, row * b_sz, b_sz+1, b_sz+1);
     }
   }
 
